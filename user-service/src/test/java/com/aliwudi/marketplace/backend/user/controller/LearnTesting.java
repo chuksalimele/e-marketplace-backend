9c0c44b204a017db5134f3542e67b69450d4c94c
Chuks Alimele <chuksalimele@gmail.com>
2025-06-12 11:06:53 +0100

    maw

---
+++ user-service/src/test/java/com/aliwudi/marketplace/backend/user/controller/LearnTesting.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aliwudi.marketplace.backend.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author user
+ */
+public class LearnTesting {
+
+    @Test
+    void testAdd() {
+        System.out.println("testAdd");
+    }
+
+    @Test
+    void testSubstract() {
+        System.out.println("testSubstract");
+    }
+}
