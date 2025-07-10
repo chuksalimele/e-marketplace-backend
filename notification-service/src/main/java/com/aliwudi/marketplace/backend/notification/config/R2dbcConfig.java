2da173aacb87572636ce865b97c32b51cd65d4ac
Chuks Alimele <chuksalimele@gmail.com>
2025-07-10 19:50:43 +0100

    maw

--- notification-service/src/main/java/com/aliwudi/marketplace/backend/notification/config/R2dbcConfig.java
+++ notification-service/src/main/java/com/aliwudi/marketplace/backend/notification/config/R2dbcConfig.java
@@ -3,10 +3,8 @@ package com.aliwudi.marketplace.backend.notification.config;
 
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
